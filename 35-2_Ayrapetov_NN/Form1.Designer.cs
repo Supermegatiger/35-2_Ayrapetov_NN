f1d084d03557b89126239ecf6d56c42b4e5a377b
Supermegatiger <tigra2820@mail.ru>
2023-09-07 00:50:18 +0300

    init

---
+++ 35-2_Ayrapetov_NN/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace _35_2_Ayrapetov_NN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.px1 = new System.Windows.Forms.Button();
+            this.px2 = new System.Windows.Forms.Button();
+            this.px3 = new System.Windows.Forms.Button();
+            this.px4 = new System.Windows.Forms.Button();
+            this.px5 = new System.Windows.Forms.Button();
+            this.px6 = new System.Windows.Forms.Button();
+            this.px7 = new System.Windows.Forms.Button();
+            this.px8 = new System.Windows.Forms.Button();
+            this.px9 = new System.Windows.Forms.Button();
+            this.px10 = new System.Windows.Forms.Button();
+            this.px11 = new System.Windows.Forms.Button();
+            this.px12 = new System.Windows.Forms.Button();
+            this.px13 = new System.Windows.Forms.Button();
+            this.px14 = new System.Windows.Forms.Button();
+            this.px15 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.recBtn = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // px1
+            // 
+            this.px1.BackColor = System.Drawing.Color.White;
+            this.px1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.px1.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.px1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.px1.Location = new System.Drawing.Point(15, 14);
+            this.px1.Margin = new System.Windows.Forms.Padding(0);
+            this.px1.Name = "px1";
+            this.px1.Size = new System.Drawing.Size(50, 50);
+            this.px1.TabIndex = 0;
+            this.px1.UseVisualStyleBackColor = false;
+            this.px1.Click += new System.EventHandler(this.px1_Click);
+            // 
+            // px2
+            // 
+            this.px2.BackColor = System.Drawing.Color.White;
+            this.px2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.px2.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.px2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.px2.Location = new System.Drawing.Point(65, 14);
+            this.px2.Margin = new System.Windows.Forms.Padding(0);
+            this.px2.Name = "px2";
+            this.px2.Size = new System.Drawing.Size(50, 50);
+            this.px2.TabIndex = 1;
+            this.px2.UseVisualStyleBackColor = false;
+            this.px2.Click += new System.EventHandler(this.px2_Click);
+            // 
+            // px3
+            // 
+            this.px3.BackColor = System.Drawing.Color.White;
+            this.px3.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.px3.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.px3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.px3.Location = new System.Drawing.Point(115, 14);
+            this.px3.Margin = new System.Windows.Forms.Padding(0);
+            this.px3.Name = "px3";
+            this.px3.Size = new System.Drawing.Size(50, 50);
+            this.px3.TabIndex = 2;
+            this.px3.UseVisualStyleBackColor = false;
+            this.px3.Click += new System.EventHandler(this.px3_Click);
+            // 
+            // px4
+            // 
+            this.px4.BackColor = System.Drawing.Color.White;
+            this.px4.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.px4.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.px4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.px4.Location = new System.Drawing.Point(15, 64);
+            this.px4.Margin = new System.Windows.Forms.Padding(0);
+            this.px4.Name = "px4";
+            this.px4.Size = new System.Drawing.Size(50, 50);
+            this.px4.TabIndex = 3;
+            this.px4.UseVisualStyleBackColor = false;
+            this.px4.Click += new System.EventHandler(this.px4_Click);
+            // 
+            // px5
+            // 
+            this.px5.BackColor = System.Drawing.Color.White;
+            this.px5.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.px5.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.px5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.px5.Location = new System.Drawing.Point(65, 64);
+            this.px5.Margin = new System.Windows.Forms.Padding(0);
+            this.px5.Name = "px5";
+            this.px5.Size = new System.Drawing.Size(50, 50);
+            this.px5.TabIndex = 4;
+            this.px5.UseVisualStyleBackColor = false;
+            this.px5.Click += new System.EventHandler(this.px5_Click);
+            // 
+            // px6
+            // 
+            this.px6.BackColor = System.Drawing.Color.White;
+            this.px6.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.px6.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.px6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.px6.Location = new System.Drawing.Point(115, 64);
+            this.px6.Margin = new System.Windows.Forms.Padding(0);
+            this.px6.Name = "px6";
+            this.px6.Size = new System.Drawing.Size(50, 50);
+            this.px6.TabIndex = 5;
+            this.px6.UseVisualStyleBackColor = false;
+            this.px6.Click += new System.EventHandler(this.px6_Click);
+            // 
+            // px7
+            // 
+            this.px7.BackColor = System.Drawing.Color.White;
+            this.px7.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.px7.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.px7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.px7.Location = new System.Drawing.Point(15, 114);
+            this.px7.Margin = new System.Windows.Forms.Padding(0);
+            this.px7.Name = "px7";
+            this.px7.Size = new System.Drawing.Size(50, 50);
+            this.px7.TabIndex = 6;
+            this.px7.UseVisualStyleBackColor = false;
+            this.px7.Click += new System.EventHandler(this.px7_Click);
+            // 
+            // px8
+            // 
+            this.px8.BackColor = System.Drawing.Color.White;
+            this.px8.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.px8.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.px8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.px8.Location = new System.Drawing.Point(65, 114);
+            this.px8.Margin = new System.Windows.Forms.Padding(0);
+            this.px8.Name = "px8";
+            this.px8.Size = new System.Drawing.Size(50, 50);
+            this.px8.TabIndex = 7;
+            this.px8.UseVisualStyleBackColor = false;
+            this.px8.Click += new System.EventHandler(this.px8_Click);
+            // 
+            // px9
+            // 
+            this.px9.BackColor = System.Drawing.Color.White;
+            this.px9.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.px9.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.px9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.px9.Location = new System.Drawing.Point(115, 114);
+            this.px9.Margin = new System.Windows.Forms.Padding(0);
+            this.px9.Name = "px9";
+            this.px9.Size = new System.Drawing.Size(50, 50);
+            this.px9.TabIndex = 8;
+            this.px9.UseVisualStyleBackColor = false;
+            this.px9.Click += new System.EventHandler(this.px9_Click);
+            // 
+            // px10
+            // 
+            this.px10.BackColor = System.Drawing.Color.White;
+            this.px10.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.px10.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.px10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.px10.Location = new System.Drawing.Point(15, 164);
+            this.px10.Margin = new System.Windows.Forms.Padding(0);
+            this.px10.Name = "px10";
+            this.px10.Size = new System.Drawing.Size(50, 50);
+            this.px10.TabIndex = 9;
+            this.px10.UseVisualStyleBackColor = false;
+            this.px10.Click += new System.EventHandler(this.px10_Click);
+            // 
+            // px11
+            // 
+            this.px11.BackColor = System.Drawing.Color.White;
+            this.px11.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.px11.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.px11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.px11.Location = new System.Drawing.Point(65, 164);
+            this.px11.Margin = new System.Windows.Forms.Padding(0);
+            this.px11.Name = "px11";
+            this.px11.Size = new System.Drawing.Size(50, 50);
+            this.px11.TabIndex = 10;
+            this.px11.UseVisualStyleBackColor = false;
+            this.px11.Click += new System.EventHandler(this.px11_Click);
+            // 
+            // px12
+            // 
+            this.px12.BackColor = System.Drawing.Color.White;
+            this.px12.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.px12.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.px12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.px12.Location = new System.Drawing.Point(115, 164);
+            this.px12.Margin = new System.Windows.Forms.Padding(0);
+            this.px12.Name = "px12";
+            this.px12.Size = new System.Drawing.Size(50, 50);
+            this.px12.TabIndex = 11;
+            this.px12.UseVisualStyleBackColor = false;
+            this.px12.Click += new System.EventHandler(this.px12_Click);
+            // 
+            // px13
+            // 
+            this.px13.BackColor = System.Drawing.Color.White;
+            this.px13.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.px13.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.px13.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.px13.Location = new System.Drawing.Point(15, 214);
+            this.px13.Margin = new System.Windows.Forms.Padding(0);
+            this.px13.Name = "px13";
+            this.px13.Size = new System.Drawing.Size(50, 50);
+            this.px13.TabIndex = 12;
+            this.px13.UseVisualStyleBackColor = false;
+            this.px13.Click += new System.EventHandler(this.px13_Click);
+            // 
+            // px14
+            // 
+            this.px14.BackColor = System.Drawing.Color.White;
+            this.px14.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.px14.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.px14.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.px14.Location = new System.Drawing.Point(65, 214);
+            this.px14.Margin = new System.Windows.Forms.Padding(0);
+            this.px14.Name = "px14";
+            this.px14.Size = new System.Drawing.Size(50, 50);
+            this.px14.TabIndex = 13;
+            this.px14.UseVisualStyleBackColor = false;
+            this.px14.Click += new System.EventHandler(this.px14_Click);
+            // 
+            // px15
+            // 
+            this.px15.BackColor = System.Drawing.Color.White;
+            this.px15.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.px15.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
+            this.px15.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.px15.Location = new System.Drawing.Point(115, 214);
+            this.px15.Margin = new System.Windows.Forms.Padding(0);
+            this.px15.Name = "px15";
+            this.px15.Size = new System.Drawing.Size(50, 50);
+            this.px15.TabIndex = 14;
+            this.px15.UseVisualStyleBackColor = false;
+            this.px15.Click += new System.EventHandler(this.px15_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel1.Controls.Add(this.recBtn);
+            this.panel1.Controls.Add(this.px11);
+            this.panel1.Controls.Add(this.px13);
+            this.panel1.Controls.Add(this.px1);
+            this.panel1.Controls.Add(this.px14);
+            this.panel1.Controls.Add(this.px2);
+            this.panel1.Controls.Add(this.px15);
+            this.panel1.Controls.Add(this.px3);
+            this.panel1.Controls.Add(this.px10);
+            this.panel1.Controls.Add(this.px6);
+            this.panel1.Controls.Add(this.px5);
+            this.panel1.Controls.Add(this.px12);
+            this.panel1.Controls.Add(this.px4);
+            this.panel1.Controls.Add(this.px7);
+            this.panel1.Controls.Add(this.px9);
+            this.panel1.Controls.Add(this.px8);
+            this.panel1.Location = new System.Drawing.Point(230, 139);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(356, 289);
+            this.panel1.TabIndex = 15;
+            // 
+            // recBtn
+            // 
+            this.recBtn.BackColor = System.Drawing.Color.ForestGreen;
+            this.recBtn.FlatAppearance.BorderSize = 0;
+            this.recBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.recBtn.Location = new System.Drawing.Point(220, 98);
+            this.recBtn.Name = "recBtn";
+            this.recBtn.Size = new System.Drawing.Size(113, 82);
+            this.recBtn.TabIndex = 16;
+            this.recBtn.Text = "RECOGNIZE";
+            this.recBtn.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button px1;
+        private System.Windows.Forms.Button px2;
+        private System.Windows.Forms.Button px3;
+        private System.Windows.Forms.Button px4;
+        private System.Windows.Forms.Button px5;
+        private System.Windows.Forms.Button px6;
+        private System.Windows.Forms.Button px7;
+        private System.Windows.Forms.Button px8;
+        private System.Windows.Forms.Button px9;
+        private System.Windows.Forms.Button px10;
+        private System.Windows.Forms.Button px11;
+        private System.Windows.Forms.Button px12;
+        private System.Windows.Forms.Button px13;
+        private System.Windows.Forms.Button px14;
+        private System.Windows.Forms.Button px15;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button recBtn;
+    }
+}
+
---
+++ 35-2_Ayrapetov_NN/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _35_2_Ayrapetov_NN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tempHandler(Button b)
+        {
+            if (b.BackColor == Color.White)
+            {
+                b.BackColor = Color.Black;
+            }
+            else
+            {
+                b.BackColor = Color.White;
+            }
+        }
+
+        private void px1_Click(object sender, EventArgs e){
+            tempHandler(px1);
+        }
+
+        private void px2_Click(object sender, EventArgs e)
+        {
+            tempHandler(px2);
+        }
+
+        private void px3_Click(object sender, EventArgs e)
+        {
+            tempHandler(px3);
+        }
+
+        private void px4_Click(object sender, EventArgs e)
+        {
+            tempHandler(px4);
+        }
+
+        private void px5_Click(object sender, EventArgs e)
+        {
+            tempHandler(px5);
+        }
+
+        private void px6_Click(object sender, EventArgs e)
+        {
+            tempHandler(px6);
+        }
+
+        private void px7_Click(object sender, EventArgs e)
+        {
+            tempHandler(px7);
+        }
+
+        private void px8_Click(object sender, EventArgs e)
+        {
+            tempHandler(px8);
+        }
+
+        private void px9_Click(object sender, EventArgs e)
+        {
+            tempHandler(px9);
+        }
+
+        private void px10_Click(object sender, EventArgs e)
+        {
+            tempHandler(px10);
+        }
+
+        private void px11_Click(object sender, EventArgs e)
+        {
+            tempHandler(px11);
+        }
+
+        private void px12_Click(object sender, EventArgs e)
+        {
+            tempHandler(px12);
+        }
+
+        private void px13_Click(object sender, EventArgs e)
+        {
+            tempHandler(px13);
+        }
+
+        private void px14_Click(object sender, EventArgs e)
+        {
+            tempHandler(px14);
+        }
+
+        private void px15_Click(object sender, EventArgs e)
+        {
+            tempHandler(px15);
+        }
+    }
+}
