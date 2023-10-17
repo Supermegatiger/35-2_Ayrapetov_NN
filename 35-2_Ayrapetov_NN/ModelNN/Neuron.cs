105c9292c495d31f155ebb841c2c7f9e1e4e8093
nothing <68782056+Supermegatiger@users.noreply.github.com>
2023-10-17 15:14:34 +0300

    Update Neuron.cs

--- 35-2_Ayrapetov_NN/ModelNN/Neuron.cs
+++ 35-2_Ayrapetov_NN/ModelNN/Neuron.cs
@@ -1,12 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static System.Math;
 
-namespace _35_2_Ayrapetov_NN.ModelNN
-{
-    internal class Neuron
-    {
+
+namespace _35_2_Ayrapetov_NN.ModelNN{
+
+    class Neuron{
+        // поля
+        private NeuroType type;
+        private double[] inputs;
+        private double[] weights;
+        private double derivative;
+
+        private double output;
+
+        // свойства 
+        public double[] Inputs
+        {
+            get { return inputs; }
+            set { inputs = value; }
+        }
+
+        public double[] Weights
+        {
+            get { return weights; }
+            set { weights = value; }
+        }
+
+        public double Derivative
+        {
+            get { return derivative; }
+        }
+
+        public double Output
+        {
+            get { return output; }
+        }
+
+        // конструктор
+
+        public Neuron(double[] ws, NeuroType t){
+            type = t;
+            weights = ws;
+        }
+
+        public void Activator(){
+            // первый элемент weights - b (порог)
+            double sum = weights[0];
+            for (int i = 0; i < inputs.Length; i++){
+                sum += inputs[i] * weights[i+1];
+            }
+
+            switch (type)
+            {
+                case NeuroType.Output:
+                    output = Exp(sum);
+                    break;
+                case NeuroType.Hidden:
+                    output = th(sum);
+                    derivative = derivator(sum);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        // функция активации (гиперболический тангенс)
+        private double th(double x){
+            return x;
+        }
+
+        // вычисление производной
+        private double derivator(double x){
+            return x;
+        }
     }
 }
