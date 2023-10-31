cefd3f7d783f12592510cc0ec0bbc1b39800a57f
nothing <68782056+Supermegatiger@users.noreply.github.com>
2023-10-31 15:32:06 +0300

    Update HiddenLayer.cs

--- 35-2_Ayrapetov_NN/ModelNN/HiddenLayer.cs
+++ 35-2_Ayrapetov_NN/ModelNN/HiddenLayer.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace _35_2_Ayrapetov_NN.ModelNN
 {
-    internal class HiddenLayer
+    class HiddenLayer : Layer
     {
+        public HiddenLayer(int non, int nopn, NeuroType nt, string name) : base(non, nopn, nt, name) { }
+        
+        public override void Recognize(Network net, Layer nextLayer)
+        {
+            double[] hidden_out = new double[Neurons.Length];
+            for (int i = 0; i < Neurons.Length; i++)
+                hidden_out[i] = Neurons[i].Output;
+            nextLayer.Data= hidden_out;
+        }
+        public override double[] BackwardPass(double[] gr_sums)
+        {
+            double[] gr_sum = new double[numOfPrevNeurons];
+            return gr_sum;
+        }
     }
 }
