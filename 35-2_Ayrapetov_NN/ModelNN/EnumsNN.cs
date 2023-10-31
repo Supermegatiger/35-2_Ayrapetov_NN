abb8925c0d3190e82c020d65029e8f188509646b
nothing <68782056+Supermegatiger@users.noreply.github.com>
2023-10-31 15:31:47 +0300

    Update EnumsNN.cs

--- 35-2_Ayrapetov_NN/ModelNN/EnumsNN.cs
+++ 35-2_Ayrapetov_NN/ModelNN/EnumsNN.cs
@@ -1,7 +1,20 @@
-﻿
+
 namespace _35_2_Ayrapetov_NN.ModelNN
 {
-    enum NeuroType
+    enum MemoryMode // режимы работы памяти
+    {
+        GET,
+        SET,
+        INIT
+    }
+    enum NetworkMode // режимы работы нейронки
+    {
+        Train,
+        Test,
+        Demo
+    }
+
+    enum NeuroType // тип нейрона
     {
         Output,
         Hidden
